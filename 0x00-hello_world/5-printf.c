b469d9b26baa44d01d1e96736131cd9a9ed2ba50
Pawlos1990 <123082236+Pawlos1990@users.noreply.github.com>
2023-02-16 14:00:12 -0800

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+
 int main() {
-    printf("Roses are red,\n");
-    printf("Violets are blue,\n");
-    printf("Sugar is sweet,\n");
-    printf("And so are you.\n");
+    printf("The beauty of a piece of art is\n");
+    printf("not just in the intricacies of its form,\n");
+    printf("nor the depth of its color,\n");
+    printf("nor the skill of its craftsman.\n");
+    printf("No, the true beauty of a piece of art\n");
+    printf("lies in its ability to stir the soul,\n");
+    printf("to inspire the mind,\n");
+    printf("and to touch the heart.\n");
     return 0;
 }
