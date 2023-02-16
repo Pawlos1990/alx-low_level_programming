88b1c8645a5d6c30a6a9045f7526e3da8d3b4417
Pawlos1990 <123082236+Pawlos1990@users.noreply.github.com>
2023-02-16 15:09:32 -0800

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,15 @@
 #include <unistd.h>
+/**
+ * Main function for the 101-quote program.
+ *
+ * This program prints a specified message to the standard error stream.
+ * It takes one command-line argument, which should be a string to print.
+ *
+ * Returns: 0 on success, or 1 if an error occurred.
+ */
+int main(int argc, char *argv[]) {
+  // your code here
+}
 
 int main(void) {
   char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
