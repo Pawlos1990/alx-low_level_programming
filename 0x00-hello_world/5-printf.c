795df6b7a722fd3decbdae756ceeeeae31853044
myUsername <myemail@gmail.com>
2023-02-16 13:36:20 -0800

    task 5 and 6

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Roses are red,\n");
+    printf("Violets are blue,\n");
+    printf("Sugar is sweet,\n");
+    printf("And so are you.\n");
+    return 0;
+}
