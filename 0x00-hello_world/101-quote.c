8a0b0f1affcdf04b473aad4ea1304ea2536cbcb2
Pawlos1990 <123082236+Pawlos1990@users.noreply.github.com>
2023-02-16 14:59:21 -0800

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,7 @@
 #include <unistd.h>
-// sizeof(message)-1 to exclude null terminator
-/* sizeof(message)-1 to exclude null terminator */
+
 int main(void) {
- char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
- write(2, message, sizeof(message)-1); // sizeof(message)-1 to exclude null terminator
- return 1;
+  char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+  write(STDERR_FILENO, message, sizeof(message) - 1);
+  return 1;
 }
