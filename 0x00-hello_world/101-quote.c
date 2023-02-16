78892933bd7e791306ce90afb28e52010b5eacf3
Pawlos1990 <123082236+Pawlos1990@users.noreply.github.com>
2023-02-16 15:15:10 -0800

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
+#include <stdio.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]) 
+/**
+ * main - A C program that prints a line to the standard error
+ * Return: 1 (Success)
+ */
+int main(void)
 {
-  // your code here
-}
-
-int main(void) {
-  char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-  
-  write(STDERR_FILENO, message, sizeof(message) - 1);
-  return 1;
+    fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+    return (1);
 }
